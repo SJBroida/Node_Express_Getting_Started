7c1870138a9b32b7ee0b0f00bbb3eb4bd5bf4489
Stephen James Broida <87044792+SJBroida@users.noreply.github.com>
2021-09-27 15:21:25 -0400

    Updated Server.js
    Added additional comments to server.js
    Broke down listener into two lines instead of one.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,25 @@
+/* With destructuring and default arguments, 
+set the variable PORT to be equal to whatever 
+value is found inside of process.env 
+or default to 5000.
+*/
 const { PORT = 5000 } = process.env;
-
+// You require the Express application that you exported.
 const app = require("./app");
+//This function will run when the server successfully starts.
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+/*
+The listen() method on your Express application is what 
+runs the server. It takes two arguments: a port number 
+and a function. The PORT variable defines where your 
+server is running, and the listener() function will 
+get called as soon as the server has successfully started.
+*/
+app.listen(PORT, listener);
+
+
+/* 
+Alternative way to do the last step
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}!`));
+*/
